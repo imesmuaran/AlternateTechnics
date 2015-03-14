e46a4614346d5c4c3975526d93236ea724b7b648
imesmuaran <imes.muaran@gmail.com>
2015-03-14 14:06:52 +0100

    BlockLoader

---
+++ src/main/java/com/imesmuaran/alternatetechnics/blocks/BlockLoader.java
@@ -0,0 +1,35 @@
+package com.imesmuaran.alternatetechnics.blocks;
+
+import com.imesmuaran.alternatetechnics.utility.LogHelper;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockLoader
+{
+
+
+
+    //** Block Init **//
+    public static Block cheeseBlock;
+    public static Block burgerBlock;
+    public static Block verticalConveyor;
+    public static Block waterwheelBase;
+    public static Block bucketBlock;
+
+    public static void Blocks()
+    {
+        cheeseBlock = new BlockCheese(Material.ground);
+        GameRegistry.registerBlock(cheeseBlock, "cheeseBlock");
+        burgerBlock = new BlockBurger();
+        GameRegistry.registerBlock(burgerBlock, "burger");
+        verticalConveyor = new VerticalConveyor(Material.iron);
+        GameRegistry.registerBlock(verticalConveyor, "verticalConveyor");
+        waterwheelBase = new BlockWaterwheelBase(Material.iron);
+        GameRegistry.registerBlock(waterwheelBase, "waterwheelBase");
+        bucketBlock = new BlockBucket(Material.iron);
+        GameRegistry.registerBlock(bucketBlock, "bucketBlock");
+
+        LogHelper.info("Blocks loaded!");
+    }
+}
