cd6298f79a96b4275f2ab81bfc22357d9e5b5f89
imesmuaran <imes.muaran@gmail.com>
2015-08-01 17:01:17 +0200

    Firepit Server Bug behoben

--- src/main/java/com/imesmuaran/alternatetechnics/blocks/BlockFirepit.java
+++ src/main/java/com/imesmuaran/alternatetechnics/blocks/BlockFirepit.java
@@ -142,7 +142,6 @@ public class BlockFirepit extends BlockContainer implements ITileEntityProvider{
     }
     
     // Lit the Fire!
-    @SideOnly(Side.CLIENT)
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
     {	
